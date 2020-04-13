681be536ec8090dee62fa3d2cafa2328791453d1
Harry Jarman <me@harryjms.uk>
2020-04-13 14:26:17 +0100

    Card stacks

--- services/public/src/components/CardStack/index.jsx
+++ services/public/src/components/CardStack/index.jsx
@@ -6,16 +6,19 @@ const useStyles = createUseStyles({
     position: "relative",
     marginLeft: 10,
     marginRight: 10,
+    transition: "width 0.2s ease-in-out",
   },
   Card: {
+    display: "inline-block",
     position: "absolute",
     boxShadow: "-1px 0 5px 0 #c5c5c5",
     borderRadius: 10,
+    transition: "left 0.2s ease-in-out",
   },
 });
 
-const CardStack = ({ children }) => {
-  const [stackWidth, setStackWidth] = useState();
+const CardStack = ({ children, spread = false, onClick }) => {
+  const [cardWidth, setCardWidth] = useState(0);
   const classes = useStyles();
   const cardRef = createRef();
   const stackRef = createRef();
@@ -23,16 +26,27 @@ const CardStack = ({ children }) => {
   useLayoutEffect(() => {
     if (stackRef.current && cardRef.current) {
       const cardWidth = cardRef.current.clientWidth;
-      const stackWidth = cardWidth + 30 * (children.length - 1) + "px";
+      let stackWidth = cardWidth + 30 * (children.length - 1) + "px";
+      if (spread) {
+        stackWidth =
+          cardWidth * children.length + 5 * (children.length - 1) + "px";
+      }
       stackRef.current.style.width = stackWidth;
       stackRef.current.style.minWidth = stackWidth;
-      console.log(cardWidth);
+      setCardWidth(cardWidth);
     }
-  }, [children]);
+  }, [children, spread]);
+
   return (
-    <div className={classes.CardStack} ref={stackRef}>
+    <div className={classes.CardStack} ref={stackRef} onClick={onClick}>
       {children.map((child, i) => (
-        <div key={i} className={classes.Card} style={{ left: 30 * i }}>
+        <div
+          key={i}
+          className={classes.Card}
+          style={{
+            left: spread ? (cardWidth + 5) * i : 30 * i,
+          }}
+        >
           {React.cloneElement(child, { innerRef: i === 0 ? cardRef : null })}
         </div>
       ))}
--- services/public/src/components/Game/index.jsx
+++ services/public/src/components/Game/index.jsx
@@ -9,24 +9,50 @@ const Game = ({ match }) => {
   const [hideCard, setHideCard] = useState(false);
   const [blackCard, setBlackCard] = useState({
     text: "I got 99 problems but _ ain't one.",
-    pick: 2,
+    pick: 1,
   });
+  const [spread, setSpread] = useState(null);
   const [whiteCards, setWhiteCards] = useState(["Dick envy"]);
+  const [playedCards, setPlayedCards] = useState([
+    "Dick envy",
+    "A gaping vagina",
+  ]);
 
+  const handleSpreadStack = (i) => {
+    if (spread !== i) {
+      setSpread(i);
+    } else {
+      setSpread(null);
+    }
+  };
   return (
     <>
       <Rail>
         <Card colour="black" hideValue={hideCard} pick={blackCard.pick}>
           {blackCard.text}
         </Card>
-        <CardStack>
-          <Card colour="white">Dick envy</Card>
-          <Card colour="white">A gaping vagina</Card>
-        </CardStack>
-        <CardStack>
-          <Card colour="white">Dick envy</Card>
-          <Card colour="white">A gaping vagina</Card>
-        </CardStack>
+        {playedCards.map((card, i) => {
+          if (Array.isArray(card)) {
+            return (
+              <CardStack
+                key={`stack-${i}`}
+                spread={spread === i}
+                onClick={() => handleSpreadStack(i)}
+              >
+                {card.map((c) => (
+                  <Card colour="white" key={c}>
+                    {c}
+                  </Card>
+                ))}
+              </CardStack>
+            );
+          }
+          return (
+            <Card colour="white" key={card}>
+              {card}
+            </Card>
+          );
+        })}
       </Rail>
       <Rail selectable>
         {whiteCards.map((card) => (
