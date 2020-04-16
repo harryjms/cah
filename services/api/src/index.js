d0b4430879d06abcd35d920517e966c9fd3838b6
Harry Jarman <me@harryjms.uk>
2020-04-16 18:45:04 +0100

    Fix routing

--- services/api/src/index.js
+++ services/api/src/index.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
 const http = require("http").createServer(app);
-
+const path = require("path");
 const io = require("socket.io")(http);
 module.exports.socketio = io;
 
@@ -26,8 +26,14 @@ io.on("connection", (socket) => {
   }
 });
 
-app.use("/api", cookieParser(), bodyParser.json(), require("./routes/api"));
 app.use(express.static("../public/dist"));
+app.use("/api", cookieParser(), bodyParser.json(), require("./routes/api"));
+app.use("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "..", "..", "public", "dist", "index.html")
+  );
+});
+
 http.listen(SERVER_PORT, () => {
   console.log("Listening on %s...", SERVER_PORT);
 });
